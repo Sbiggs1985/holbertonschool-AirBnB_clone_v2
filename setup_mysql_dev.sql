c54db54d264826405b5799b086e456107b44ebdb
MikeBilbee <cmbilbee@gmail.com>
2023-07-01 09:51:17 -0700

    Mike 2.5

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,8 +4,6 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
-USE hbnb_dev_db;
-
 CREATE USER IF NOT EXISTS 'hbnb_dev_db'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
