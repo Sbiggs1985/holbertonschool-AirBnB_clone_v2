add77b0315bc0734ec7c5e944606ca6c447b5019
MikeBilbee <cmbilbee@gmail.com>
2023-07-01 09:48:58 -0700

    Mike 2.4'

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -11,3 +11,5 @@ CREATE USER IF NOT EXISTS 'hbnb_dev_db'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
