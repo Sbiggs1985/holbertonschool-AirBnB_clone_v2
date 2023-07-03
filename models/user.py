3c5b3b49c61f46bbac6b2281740be1d0b09a2581
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2023-07-03 11:55:50 -0500

    Update user.py

--- models/user.py
+++ models/user.py
@@ -12,13 +12,21 @@
 """Above is the original code"""
 
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from models.place import Place
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.review import Review
+
 
 class User(BaseModel, Base):
-    """ User class """
+    """This class defines a user by various attributes"""
     __tablename__ = 'users'
-
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship('Place', backref='user',
+                          cascade='all, delete-orphan')
+    reviews = relationship('Review', backref='user',
+                           cascade='all, delete-orphan')
