ade47ce4946d655c30ba8cb48be2e96e7e017c2e
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2023-07-03 11:46:59 -0500

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 """This module defines a class"""
-from models.base_model import BaseModel
+"""from models.base_model import BaseModel"""
 
-class User(BaseModel):
+"""class User(BaseModel):"""
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    """email = ''"""
+    """password = ''"""
+    """first_name = ''"""
+    """last_name = ''"""
+
+"""Above is the original code"""
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+
+class User(BaseModel, Base):
+    """ User class """
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
