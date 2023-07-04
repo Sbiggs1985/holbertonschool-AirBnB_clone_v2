64669ea52c46e0bf72eb7103075e367f304cf1db
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-07-04 10:16:58 -0700

    Updates

--- models/review.py
+++ models/review.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """ Imports the BaseModel and Base classes """
 from models.base_model import BaseModel, Base
-""" Imports the SQLalclchemy classes for defining columns """
 from sqlalchemy import Column, String, ForeignKey
 
-""" Defines a new class called 'Review' """
+
 class Review(BaseModel, Base):
     __tablename__ = 'reviews'
 
-""" Defines the column's named text, p;ace_id, and user_id """
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
