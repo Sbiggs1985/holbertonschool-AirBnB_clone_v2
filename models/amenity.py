5cb1a314561e87ffdb9c09455cd3344054c1d116
MikeBilbee <cmbilbee@gmail.com>
2023-07-04 10:22:11 -0700

    Mike 10.1

--- models/amenity.py
+++ models/amenity.py
@@ -7,6 +7,6 @@ from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
     from models.place import place_amenity
-    __tablename__="amenities"
+    __tablename__ = "amenities"
     name = Column(String(128), nullible=False)
-    place_amenities = relationship("Place", secondary=place_amenity, backref="amenities")
+    place_amenities = relationship('Place', secondary=place_amenity)
