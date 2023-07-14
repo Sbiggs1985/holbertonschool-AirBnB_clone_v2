f04388a4f977a1a93f328de86314506578593879
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-07-14 13:00:54 -0500

    Updates

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,11 +4,14 @@ from flask import Flask
 # Imports Flask
 
 app = Flask(__name__)
+# Creates Flask application instance
+# __name__ argument represents the name of the current module
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
+# @app.route is the decorator provided by Flask
 
-
-if __name__ =='__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+# Starts the Flask development server
