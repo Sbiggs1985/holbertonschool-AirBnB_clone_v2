fb6146081cc693d70e342c1bc3df58bd193d7a1a
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-07-15 11:56:16 -0500

    Update

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,20 +5,17 @@ Write a script that starts aa Flask Web Application
 
 from flask import Flask
 
-# Creates an instance of Flask
 app = Flask(__name__)
 
-# decorator that specifies the URL route
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
-# decorator deines route
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
 
 
-# Condition statement - Starts Flask development server
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
